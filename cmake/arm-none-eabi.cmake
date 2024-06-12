17a9887412ea85299d750d4a8b19ba4a1587bb83
Hermann Core <hermannoenpause@gmail.com>
2024-06-12 08:53:56 +0100

    the project now builds only with the TI ARM toolchain

--- cmake/arm-none-eabi.cmake
+++ cmake/arm-none-eabi.cmake
@@ -1,8 +1,8 @@
 ########################################################################
 # -------------------------------------------------------------------- #
-# | Author : Awatsa Hermann          |     Version : 1.0             | #
+# | Author : Awatsa Hermann          |     Version : 1.1             | #
 # -------------------------------------------------------------------- #
-# | Date : 07.02.2023                |     File: arm-none-eabi.cmake | #
+# | Date : 06.12.2024                |     File: arm-none-eabi.cmake | #
 # ---------------------------------------------------------------------#
 # | Brief : This file is use to configure the ARM  toolchain         | #
 # -------------------------------------------------------------------- #
@@ -10,9 +10,8 @@
 
 # Find a toolchain installation to use
 file(GLOB TOOLCHAIN_DIRECTORIES
-    "$ENV{TI_TOOLCHAIN_PATH}"
-    # "$ENV{GNU_TOOLCHAIN_PATH}"
-    "/opt/GNU Arm Embedded Toolchain/12.2-rel1/bin"
+    "C:/TI_LLVM_Arm_Embedded_Toolchain/bin"
+    "/home/hermann-core/TI_LLVM_ARM_TOOLCHAIN/bin"
 )
 
 # Count the available toolchains
@@ -22,9 +21,6 @@ if(TOOLCHAIN_DIRECTORIES_COUNT LESS 1)
     message(FATAL_ERROR "Could not find an ARM toolchain installation. Please install a toolchain.")
 else()
     list(GET TOOLCHAIN_DIRECTORIES 0 TOOLCHAIN_DIRECTORY)
-    if (TOOLCHAIN_DIRECTORIES_COUNT GREATER 1)
-        message(STATUS "Found multiple ARM toolchain installations. Using the default \"${TOOLCHAIN_DIRECTORY}\".")
-    endif()
 endif()
 
 # Set the suffix of executables if on Windows
@@ -35,30 +31,18 @@ endif()
 set(CMAKE_SYSTEM_NAME                  Generic)
 set(CMAKE_SYSTEM_PROCESSOR             ARM)
 
-# If the toolchain is the Texas Instruments ARM Clang toolchain
-if(TOOLCHAIN_DIRECTORY PATH_EQUAL      "$ENV{TI_TOOLCHAIN_PATH}")
-    set(TOOLCHAIN_PREFIX               "tiarm")
-# If the toolchain is the GCC embedded toolchain
-elseif(TOOLCHAIN_DIRECTORY PATH_EQUAL  "$ENV{GNU_TOOLCHAIN_PATH}")
-    set(TOOLCHAIN_PREFIX               "arm-none-eabi-")
-endif()
+set(TOOLCHAIN_PREFIX                   "tiarm")
 
 if(DEFINED TOOLCHAIN_DIRECTORY)
     set(TOOLCHAIN_PREFIX               "${TOOLCHAIN_DIRECTORY}/${TOOLCHAIN_PREFIX}")
 endif()
 
 set(ASM_OPTIONS                        "-x assembler-with-cpp")
-set(CPP_OPTIONS                        "-fno-rtti -fno-exceptions -fno-threadsafe-statics -fno-unwind-tables")
+set(CPP_OPTIONS                        "-fno-rtti -fno-exceptions -fno-threadsafe-statics")
 
-if(TOOLCHAIN_DIRECTORY PATH_EQUAL     "$ENV{TI_TOOLCHAIN_PATH}")
-    set(CMAKE_C_COMPILER               ${TOOLCHAIN_PREFIX}clang${TOOLCHAIN_SUFFIX})
-    set(CMAKE_ASM_COMPILER             ${TOOLCHAIN_PREFIX}clang${TOOLCHAIN_SUFFIX} ${ASM_OPTIONS})
-    set(CMAKE_CXX_COMPILER             ${TOOLCHAIN_PREFIX}clang${TOOLCHAIN_SUFFIX} ${CPP_OPTIONS})
-elseif(TOOLCHAIN_DIRECTORY PATH_EQUAL "$ENV{GNU_TOOLCHAIN_PATH}")
-    set(CMAKE_C_COMPILER               ${TOOLCHAIN_PREFIX}gcc${TOOLCHAIN_SUFFIX})
-    set(CMAKE_ASM_COMPILER             ${TOOLCHAIN_PREFIX}gcc${TOOLCHAIN_SUFFIX} ${ASM_OPTIONS})
-    set(CMAKE_CXX_COMPILER             ${TOOLCHAIN_PREFIX}g++${TOOLCHAIN_SUFFIX} ${CPP_OPTIONS})
-endif()
+set(CMAKE_C_COMPILER                   ${TOOLCHAIN_PREFIX}clang${TOOLCHAIN_SUFFIX})
+set(CMAKE_ASM_COMPILER                 ${TOOLCHAIN_PREFIX}clang${TOOLCHAIN_SUFFIX} ${ASM_OPTIONS})
+set(CMAKE_CXX_COMPILER                 ${TOOLCHAIN_PREFIX}clang${TOOLCHAIN_SUFFIX} ${CPP_OPTIONS})
 
 set(CMAKE_OBJCOPY                      ${TOOLCHAIN_PREFIX}objcopy${TOOLCHAIN_SUFFIX})
 set(CMAKE_SIZE                         ${TOOLCHAIN_PREFIX}size${TOOLCHAIN_SUFFIX})
